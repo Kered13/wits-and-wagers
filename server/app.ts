836ae532d6d522c84e820528d3a99a6b140536aa
Derek <none@none>
2025-01-07 06:47:43 -0500

    Add cors and body-parser to Express server.

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,22 @@
+import bodyParser = require("body-parser");
+import cors = require("cors");
 import express = require("express");
+import path = require("path");
 
 const app = express();
 
-app.listen(3000, () => {
-	console.log("Server is running on port 3000");
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+	res.send("Hello, World!");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+	console.log("Server is running on port " + port);
 });
